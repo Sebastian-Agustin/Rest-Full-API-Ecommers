a048ddc29606b0304582264320fbe2f10d94a3a5
Sebastian Miño <sebastianagustinmino@gmail.com>
2025-04-29 12:52:22 -0300

    manejando los protocolos http

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
-require("dotenv").config();
-
 const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
+//App es el objeto que devuelve la funcion de express. 
+//App tiene como metodo get. que usa para el routing de la pagina, a get se le especifica la url, y la respuesta del servidor
+//res.send es para mandar el tipo de informacion al programa
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("API de e-commerce funcionando 🚀");
+ res.send("Hola este es el root central")
 });
 
+app.post("/productos", (req, res)=>{
+    res.send("Creando productos")
+})
+app.put("/productos", (req, res)=>{
+    res.send("Actualizando todos los datos del producto")
+})
+app.patch("/productos", (req, res) =>{
+    res.send("Actualizando una parte del producto")
+})
+app.delete("/productos", (req, res) =>{
+    res.send("eliminando producto")
+})
 
 app.listen(PORT, () => {
 console.log(`Servidor escuchando en el puerto ${PORT}`);
